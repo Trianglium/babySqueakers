e9d005dd1cfc25878f66edde50692391d8974ca2
Trianglium <jbrockerrudow@gmail.com>
2022-04-02 06:51:56 -0500

    Implement services for product and promo

--- babyShoes/src/app/services/product.service.ts
+++ babyShoes/src/app/services/product.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Product } from '../shared/product';
+import { PRODUCTS } from '../shared/productes';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,17 @@ import { Injectable } from '@angular/core';
 export class ProductService {
 
   constructor() { }
+
+  getProducts(): Product[] {
+    return PRODUCTS;
+  }
+
+  getProduct(id: string): Product {
+    return PRODUCTS.filter((product) => (product.id === id))[0];
+  }
+
+  getFeaturedProduct(): Product {
+    return PRODUCTS.filter((product) => product.featured)[0];
+  }
+
 }
--- babyShoes/src/app/services/promotion.service.ts
+++ babyShoes/src/app/services/promotion.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Leader } from '../shared/leader';
+import { LEADERS } from '../shared/leaders';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,17 @@ import { Injectable } from '@angular/core';
 export class PromotionService {
 
   constructor() { }
+
+  getPromotions(): Promotion[] {
+    return PROMOTIONS;
+  }
+
+  getPromotion(id: string): Promotion {
+    return PROMOTIONS.filter((promotion) => (promotion.id === id))[0];;
+  }
+
+  getFeaturedPromotion(): Promotion {
+    return PROMOTIONS.filter((promotion) => promotion.featured)[0];
+  }
+  // Since multiple promotions will be featured at once - list index should be removed.
 }
